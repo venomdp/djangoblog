6d6b5f19afe08592b02a87c520288000911bcbf6
venomdp <35393969+venomdp@users.noreply.github.com>
2018-01-31 21:11:25 -0600

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Documents\Homework\DistTech\DjangoGirls\djangoblog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
